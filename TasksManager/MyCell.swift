411fb4d6be4feb7a626fc4ddae193c57b295884b
Stanislav <lolka.riddle@gmail.com>
2019-01-18 14:01:46 +0200

    add test

--- TasksManager/MyCell.swift
+++ TasksManager/MyCell.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import UIKit
+
+class MyCell: UITableViewCell {
+    
+    static let reuseIdentifier = "cell"
+    
+    let todoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    let todoLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.gray
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setup()
+    }
+    
+    func setup() {
+        self.addSubview(todoImageView)
+        self.addSubview(todoLabel)
+        
+        todoImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        todoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        todoImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        todoImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        todoLabel.leftAnchor.constraint(equalTo: todoImageView.rightAnchor, constant: 15).isActive = true
+        todoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- TasksManager/ViewController.swift
+++ TasksManager/ViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    var realm: Realm!
+    
+    var objectsArray: Results<Item> {
+        get {
+            return realm.objects(Item.self)
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        realm = try! Realm()
+        
+        setup()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MyCell.self, forCellReuseIdentifier: MyCell.reuseIdentifier)
+    }
+    
+  
+    func setup() {
+        view.backgroundColor = .white
+        navigationItem.title = "Tasks"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(title: "Add", style: .done, target: self, action: #selector(addNewItem))
+        navigationItem.rightBarButtonItem = addButton
+        navigationController?.navigationBar.tintColor = UIColor.black
+        
+    }
+    
+    
+    @objc func addNewItem() {
+        let alertVC = UIAlertController(title: "Add New Task", message: "Set task name", preferredStyle: .alert)
+        alertVC.addTextField(configurationHandler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        let addAction = UIAlertAction(title: "Add", style: .default) { (UIAlertAction) in
+            let myTextField = (alertVC.textFields?.first)! as UITextField
+            
+            let item = Item()
+            item.name = myTextField.text!
+            
+            try! self.realm.write {
+                self.realm.add(item)
+                self.tableView.insertRows(at: [IndexPath(row: self.objectsArray.count - 1, section: 0)], with: .automatic)
+            }
+        }
+        alertVC.addAction(cancelAction)
+        alertVC.addAction(addAction)
+        present(alertVC, animated: true, completion: nil)
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objectsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:MyCell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyCell
+        let item = objectsArray[indexPath.row]
+        cell.todoLabel.text = item.name
+        cell.todoImageView.image = UIImage(named: item.picture)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let item = objectsArray[indexPath.row]
+            
+            try! self.realm.write {
+                self.realm.delete(item)
+            }
+            
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 
 }
 
